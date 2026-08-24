3e32b9921277350e0a85ec807f488c7b3489c562
Ioannidis Ioannis <109786525+iioannidis94@users.noreply.github.com>
2026-08-24 12:28:54 +0300

    Update weapons.js

--- data/weapons.js
+++ data/weapons.js
@@ -1,1 +1,15 @@
 
+export const weapons = [
+    { 
+        id: 'w_spear_2', 
+        name: 'Pebblet Spear Tier II', 
+        sprite: 'assets/sprites/weapons/pebblet_spear.png',
+        description: 'Βασικό δόρυ για την αρχή του παιχνιδιού.'
+    },
+    { 
+        id: 'w_mint_mace', 
+        name: 'Mint Mace', 
+        sprite: 'assets/sprites/weapons/mint_mace.png',
+        description: 'Δυνατό όπλο με damage από μέντα.'
+    }
+];
