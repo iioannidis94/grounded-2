5ff772291d0f9f7a1645230d2aea0a06f01335a4
Ioannidis Ioannis <109786525+iioannidis94@users.noreply.github.com>
2026-08-24 12:29:06 +0300

    Update omni_tools.js

--- data/omni_tools.js
+++ data/omni_tools.js
@@ -1,1 +1,8 @@
-
+export const omniTools = [
+    {
+        id: 'ot_scanner',
+        name: 'Resource Scanner Upgrade',
+        sprite: 'assets/sprites/tools/scanner.png',
+        description: 'Αναβάθμιση για να σκανάρεις πόρους από μακριά.'
+    }
+];
